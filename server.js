e1550d235e03d70701e309cef270e8fbf3fda9c3
Christian Nicolini <nicolini.christian@gmail.com>
2018-11-26 18:31:58 -0500

    Added CORS configuration

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ mongoose.connect('mongodb://localhost/CustomerDB');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 // Register Routes
 var routes = require('./api/routes/customerRoutes');
 routes(app);
